a747045f7c194e614a509b34b6a893589e5f0164
Riya Parihar <riya.parihar2507@gmail.com>
2026-02-10 13:30:39 +0530

    Emergency: Enable Allow-All CORS

--- server/index.js
+++ server/index.js
@@ -14,13 +14,9 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: [
-      "http://localhost:5173", // Localhost
-      "https://critic-two.vercel.app/", // <--- ADD YOUR VERCEL URL
-    ],
+    origin: "*",
     methods: ["GET", "POST", "PUT", "DELETE"], // Allow these actions
     allowedHeaders: ["Content-Type", "Authorization"], // Allow the Token header
-    credentials: true,
   }),
 );
 
@@ -31,10 +27,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: [
-      "http://localhost:5173", // Localhost
-      "https://critic-two.vercel.app/", // <--- ADD YOUR VERCEL URL
-    ],
+    origin: "*",
     methods: ["GET", "POST"],
   },
 });
